78bbf4841d2d5e3e8bd613530aa1af06cee8e18b
gonzo <blodnasir@gmail.com>
2017-05-26 17:45:53 +0200

    solo and multi

--- ai.go
+++ ai.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 	"math/rand"
+	"time"
 )
 
 func aiPlay() {
 	log.Println("AI PLAY")
-	g.board[rand.Intn(19)][rand.Intn(19)] = 1
+	g.Board[rand.Intn(19)][rand.Intn(19)] = 1
+	time.Sleep(10000 * time.Millisecond)
 }
--- main.go
+++ main.go
@@ -8,15 +8,22 @@ import (
 )
 
 type Gomoku struct {
-	board [19][19]int
+	Board [19][19]int
+	Mode  string
 }
 
 var g Gomoku
+var player string
+var c int
 
 type coord struct {
 	X      int
 	Y      int
 	Player string
+}
+
+type start struct {
+	Mode string
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -25,10 +32,27 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 func reset(w http.ResponseWriter, r *http.Request) {
 	resetBoard()
+	c = 0
 	w.WriteHeader(200)
 }
 
+func startGame(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var t start
+	err := decoder.Decode(&t)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+	g.Mode = t.Mode
+
+}
+
 func play(w http.ResponseWriter, r *http.Request) {
+	if g.Mode == "" {
+		http.Error(w, "No mode selected yet", 400)
+		return
+	}
 	decoder := json.NewDecoder(r.Body)
 	var t coord
 	err := decoder.Decode(&t)
@@ -36,21 +60,41 @@ func play(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	defer r.Body.Close()
-	if g.board[t.X][t.Y] == 2 || g.board[t.X][t.Y] == 1 {
-		http.Error(w, "Already played here", 400)
+	if t.X < 0 || t.X > 18 || t.Y < 0 || t.Y > 18 {
+		http.Error(w, "Out of board range", 400)
+		return
 	}
-	g.board[t.X][t.Y] = 2
-	aiPlay()
+	if g.Board[t.X][t.Y] == 2 || g.Board[t.X][t.Y] == 1 {
+		http.Error(w, "Already played here", 400)
+		return
+	}
+	if player == t.Player {
+		http.Error(w, "Already Played", 400)
+		return
+	}
+	player = t.Player
+	if g.Mode == "multi" {
+		if c%2 == 0 {
+			g.Board[t.X][t.Y] = 2
+		} else {
+			g.Board[t.X][t.Y] = 1
+		}
+		c++
+	} else if g.Mode == "solo" {
+		g.Board[t.X][t.Y] = 2
+		aiPlay()
+		player = "AI"
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
-	json.NewEncoder(w).Encode(g.board)
+	json.NewEncoder(w).Encode(g.Board)
 
 }
 
 func resetBoard() {
-	for x := range g.board {
-		for y := range g.board {
-			g.board[x][y] = 0
+	for x := range g.Board {
+		for y := range g.Board {
+			g.Board[x][y] = 0
 		}
 	}
 }
@@ -60,6 +104,7 @@ func main() {
 	resetBoard()
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/play", play)
+	http.HandleFunc("/startgame", startGame)
 	http.HandleFunc("/reset", reset)
 	http.ListenAndServe(":8000", nil)
 }
