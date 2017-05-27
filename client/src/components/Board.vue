a4d1f224e4320477e739fb932f2c221924684ddb
abombard <arthbab@gmail.com>
2017-05-27 15:12:42 +0200

    there is a board !

--- client/src/components/Board.vue
+++ client/src/components/Board.vue
@@ -1,11 +1,11 @@
 <template>
  	<div class="Board">
-		<div id="Line"
-			v-for="(line, x) in board">
-			<div id="Cell"
-				v-for="(cell, y) in line"
+		<div class="Row"
+			v-for="(row, x) in board">
+			<div class="Cell"
+				v-for="(cell, y) in row"
 				v-on:click="cellOnClick(x, y)">
-				this is a Cell
+   				{{ cell }}
    			</div>
    		</div>
 	</div>
@@ -18,3 +18,25 @@
 	}
 
 </script>
+
+<style scoped>
+.Board {
+	border: 3px solid #fff;
+	width: 70vh;
+	display: flex;
+	height: 70vh;
+	margin: auto;
+	flex-flow: row nowrap;
+}
+
+.Row {
+	flex: 1;
+	display: flex;
+	flex-flow: column nowrap;
+}
+
+.Cell {
+	border: 1px solid #000;
+	flex: 1;
+}
+</style>
