354c2619ac608d01a4dbac823d81c5ec5789f2f5
gonzo <blodnasir@gmail.com>
2017-05-27 15:13:24 +0200

    serve with go

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 type Gomoku struct {
@@ -48,6 +53,20 @@ func startGame(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func checkDoubleThree(t coord) error {
+
+	return nil
+}
+
+func processPlay(t coord) error {
+	ret := checkDoubleThree(t)
+	if ret != nil {
+		return ret
+	}
+
+	return ret
+}
+
 func play(w http.ResponseWriter, r *http.Request) {
 	if g.Mode == "" {
 		http.Error(w, "No mode selected yet", 400)
@@ -85,10 +104,10 @@ func play(w http.ResponseWriter, r *http.Request) {
 		aiPlay()
 		player = "AI"
 	}
+	processPlay(t)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
 	json.NewEncoder(w).Encode(g.Board)
-
 }
 
 func resetBoard() {
@@ -100,11 +119,63 @@ func resetBoard() {
 }
 
 func main() {
-	log.Println("Start")
 	resetBoard()
-	http.HandleFunc("/", hello)
-	http.HandleFunc("/play", play)
-	http.HandleFunc("/startgame", startGame)
-	http.HandleFunc("/reset", reset)
-	http.ListenAndServe(":8000", nil)
+	var entry string
+	var static string
+	var port string
+
+	flag.StringVar(&entry, "entry", "./client/index.html", "the entrypoint to serve.")
+	flag.StringVar(&static, "static", ".", "the directory to serve static files from.")
+	flag.StringVar(&port, "port", "8000", "the `port` to listen on.")
+	flag.Parse()
+
+	r := mux.NewRouter()
+
+	// Note: In a larger application, we'd likely extract our route-building logic into our handlers
+	// package, given the coupling between them.
+
+	// It's important that this is before your catch-all route ("/")
+	api := r.PathPrefix("/api/v1/").Subrouter()
+	api.HandleFunc("/startgame", startGame).Methods("POST")
+	// Optional: Use a custom 404 handler for our API paths.
+	// api.NotFoundHandler = JSONNotFound
+
+	// Serve static assets directly.
+	r.PathPrefix("/dist").Handler(http.FileServer(http.Dir(static)))
+
+	// Catch-all: Serve our JavaScript application's entry-point (index.html).
+	r.PathPrefix("/").HandlerFunc(IndexHandler(entry))
+
+	srv := &http.Server{
+		Handler: handlers.LoggingHandler(os.Stdout, r),
+		Addr:    "127.0.0.1:" + port,
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func IndexHandler(entrypoint string) func(w http.ResponseWriter, r *http.Request) {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, entrypoint)
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]interface{}{
+		"id": "12345",
+		"ts": time.Now().Format(time.RFC3339),
+	}
+
+	b, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	w.Write(b)
 }
