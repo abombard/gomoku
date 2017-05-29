f1ecaea52a2e36c876fdcf258cb0c396dbd89bf1
abombard <arthbab@gmail.com>
2017-05-29 11:44:09 +0200

    implement rules

--- main.go
+++ main.go
@@ -78,20 +78,6 @@ func startGame(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func checkDoubleThree(t coord) error {
-
-	return nil
-}
-
-func processPlay(t coord) error {
-	ret := checkDoubleThree(t)
-	if ret != nil {
-		return ret
-	}
-
-	return ret
-}
-
 func play(w http.ResponseWriter, r *http.Request) {
 	if g.Mode == "" {
 		http.Error(w, "No mode selected yet", 400)
@@ -104,25 +90,17 @@ func play(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	defer r.Body.Close()
-	if t.X < 0 || t.X > 18 || t.Y < 0 || t.Y > 18 {
-		http.Error(w, "Out of board range", 400)
+	err = IsValidMove(t)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
 		return
 	}
-	if g.Board[t.X][t.Y] == 2 || g.Board[t.X][t.Y] == 1 {
-		http.Error(w, "Already played here", 400)
-		return
-	}
-	if t.Player != players[current] {
-		http.Error(w, "Not your turn "+t.Player+" != "+players[current], 400)
-		return
-	}
-	g.Board[t.X][t.Y] = current + 1
+	Move(t)
 	if g.Mode == "solo" {
 		aiPlay()
 	} else if g.Mode == "multi" {
 		current = (current + 1) % 2
 	}
-	processPlay(t)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
 	json.NewEncoder(w).Encode(g.Board)
--- rules.go
+++ rules.go
@@ -1,11 +1,13 @@
 package main
 
+import "fmt"
+
 const (
 	WIDTH = 19
 	HEIGHT = 19
 )
 
-func isValidCoord(x, y int) bool {
+func IsValidCoord(x, y int) bool {
 	return x >= 0 && x < HEIGHT && y >= 0 && y < WIDTH
 }
 
@@ -21,34 +23,7 @@ func isFree(x, y int) bool {
 	return g.Board[x][y] == 0
 }
 
-func capture(x, y int, p int) {
-
-	checks := [8][3][2]int{
-		{ { -1, -1 }, { -2, -2 }, { -3, -3 } },
-		{ { -1,  0 }, { -2,  0 }, { -3,  0 } },
-		{ { -1,  1 }, { -2,  2 }, { -3,  3 } },
-		{ {  0,  1 }, {  0,  2 }, {  0,  3 } },
-		{ {  1,  1 }, {  2,  2 }, {  3,  3 } },
-		{ {  1,  0 }, {  2,  0 }, {  3,  0 } },
-		{ {  1, -1 }, {  2, -2 }, {  3, -3 } },
-		{ {  0, -1 }, {  0, -2 }, {  0, -3 } },
-	}
-	for i := range checks {
-		p1, p2, p3 := checks[i][0], checks[i][1], checks[i][2]
-		x1, y1 := x + p1[0], y + p1[1]
-		x2, y2 := x + p2[0], y + p2[1]
-		x3, y3 := x + p3[0], y + p3[1]
-		if isValidCoord(x1, y1) && isValidCoord(x2, y2) && isValidCoord(x3, y3) {
-			if isEnemy(x1, y1) && isEnemy(x2, y2) && isMe(x3, y3) {
-				// Successfully captured
-				g.Board[x1][y1] = 0
-				g.Board[x2][y2] = 0
-			}
-		}
-	}
-}
-
-func introduceDoubleThree(x, y int, p int) bool {
+func introduceDoubleThree(x, y int) bool {
 
 	checks := [4][6][2]int{
 		{ { -3, -3 }, { -2, -2 }, { -1, -1 }, {  1,  1 }, {  2,  2 }, {  3,  3 } },
@@ -69,7 +44,7 @@ func introduceDoubleThree(x, y int, p int) bool {
 				}
 			}
 			x1, y1 := checks[i][j][0], checks[i][j][1]
-			if isValidCoord(x1, y1) {
+			if IsValidCoord(x1, y1) {
 				if isMe(x1, y1) {
 					blank = 0
 					count += 1
@@ -92,7 +67,56 @@ func introduceDoubleThree(x, y int, p int) bool {
 		}
 	}
 
-	return threeCount < 2
+	return threeCount > 1
+}
+
+func IsValidMove(c coord) error {
+	x, y := c.X, c.Y
+	if !IsValidCoord(x, y) {
+		return fmt.Errorf("Invalid coordonate")
+	} else if !isFree(x, y) {
+		return fmt.Errorf("Case already taken")
+	} else if introduceDoubleThree(x, y) {
+		return fmt.Errorf("Illegal move")
+	}
+	return nil
+}
+
+func tryCapture(x, y int) {
+
+	checks := [8][3][2]int{
+		{ { -1, -1 }, { -2, -2 }, { -3, -3 } },
+		{ { -1,  0 }, { -2,  0 }, { -3,  0 } },
+		{ { -1,  1 }, { -2,  2 }, { -3,  3 } },
+		{ {  0,  1 }, {  0,  2 }, {  0,  3 } },
+		{ {  1,  1 }, {  2,  2 }, {  3,  3 } },
+		{ {  1,  0 }, {  2,  0 }, {  3,  0 } },
+		{ {  1, -1 }, {  2, -2 }, {  3, -3 } },
+		{ {  0, -1 }, {  0, -2 }, {  0, -3 } },
+	}
+	for i := range checks {
+		p1, p2, p3 := checks[i][0], checks[i][1], checks[i][2]
+		x1, y1 := x + p1[0], y + p1[1]
+		x2, y2 := x + p2[0], y + p2[1]
+		x3, y3 := x + p3[0], y + p3[1]
+		if IsValidCoord(x1, y1) && IsValidCoord(x2, y2) && IsValidCoord(x3, y3) {
+			if isEnemy(x1, y1) && isEnemy(x2, y2) && isMe(x3, y3) {
+				// Successfully captured
+				g.Board[x1][y1] = 0
+				g.Board[x2][y2] = 0
+			}
+		}
+	}
+}
+
+func Move(c coord) {
+	err := IsValidMove(c)
+	if err != nil {
+		return
+	}
+	x, y := c.X, c.Y
+	tryCapture(x, y)
+	g.Board[x][y] = current + 1
 }
 
 func canBeCaptured(x, y int) bool {
@@ -109,7 +133,7 @@ func canBeCaptured(x, y int) bool {
 		x2, y2 := x + p2[0], y + p2[1]
 		x3, y3 := x + p3[0], y + p3[1]
 		x4, y4 := x + p4[0], y + p4[1]
-		if isValidCoord(x1, y2) && isValidCoord(x2, y2) && isValidCoord(x3, y3) {
+		if IsValidCoord(x1, y2) && IsValidCoord(x2, y2) && IsValidCoord(x3, y3) {
 			if (isMe(x2, y2) && ((isFree(x1, y1) && isEnemy(x3, y3)) || (isEnemy(x1, y1) && isFree(x3, y3)))) ||
 			   (isMe(x3, y3) && ((isFree(x2, y2) && isEnemy(x4, y4)) || (isEnemy(x2, y2) && isFree(x4, y4)))) {
 				return true
@@ -119,7 +143,8 @@ func canBeCaptured(x, y int) bool {
 	return false
 }
 
-func isGameOver(x, y int) bool {
+func IsGameOver(c coord) bool {
+	x, y := c.X, c.Y
 
 	if canBeCaptured(x, y) {
 		return false
@@ -135,7 +160,7 @@ func isGameOver(x, y int) bool {
 		count := 0
 		for j := range checks[i] {
 			x1, y1 := checks[i][j][0], checks[i][j][1]
-			if isValidCoord(x1, y1) && isMe(x1, y1) && !canBeCaptured(x1, y1) {
+			if IsValidCoord(x1, y1) && isMe(x1, y1) && !canBeCaptured(x1, y1) {
 				count += 1
 				if count == 4 {
 					return true
