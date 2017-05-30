3ac906164c67a1fa99c55296fda6bc078a89d6d3
gonzo <blodnasir@gmail.com>
2017-05-30 18:46:31 +0200

    heursitc

--- heuristic_aduban.go
+++ heuristic_aduban.go
@@ -1,7 +1,5 @@
 package main
 
-import "log"
-
 func maxval(a, b, c, d int) int {
 	best := 0
 	if a > best {
@@ -19,7 +17,9 @@ func maxval(a, b, c, d int) int {
 	return best
 }
 
-func heuristic2(board [][]int, player int) int {
+type fnc func(x, y int, tmp [][]int, p int) bool
+
+func getScore(board [][]int, player int, fun fnc) int {
 	b := make([][]int, HEIGHT)
 	for i := 0; i < HEIGHT; i++ {
 		b[i] = make([]int, WIDTH)
@@ -30,13 +30,13 @@ func heuristic2(board [][]int, player int) int {
 	score := 0
 	for x := 0; x < WIDTH; x++ {
 		for y := 0; y < HEIGHT; y++ {
-			if isMeNew(x, y, board, player) {
+			if fun(x, y, board, player) {
 				tmpx := x
 				tmpy := y
 				horScore := 0
 				horSpace := 0
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx++ {
-					if isMeNew(tmpx, tmpy, board, player) {
+				for ; isValidCoord(tmpx, tmpy) && (fun(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx++ {
+					if fun(tmpx, tmpy, board, player) {
 						horScore++
 						horSpace++
 					}
@@ -45,8 +45,8 @@ func heuristic2(board [][]int, player int) int {
 					}
 				}
 				tmpx = x
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx-- {
-					if isMeNew(tmpx, tmpy, board, player) {
+				for ; isValidCoord(tmpx, tmpy) && (fun(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx-- {
+					if fun(tmpx, tmpy, board, player) {
 						horScore++
 						horSpace++
 					}
@@ -57,8 +57,8 @@ func heuristic2(board [][]int, player int) int {
 				tmpx = x
 				verScore := 0
 				verSpace := 0
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpy++ {
-					if isMeNew(tmpx, tmpy, board, player) {
+				for ; isValidCoord(tmpx, tmpy) && (fun(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpy++ {
+					if fun(tmpx, tmpy, board, player) {
 						verScore++
 						verSpace++
 					}
@@ -67,8 +67,8 @@ func heuristic2(board [][]int, player int) int {
 					}
 				}
 				tmpy = y
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpy-- {
-					if isMeNew(tmpx, tmpy, board, player) {
+				for ; isValidCoord(tmpx, tmpy) && (fun(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpy-- {
+					if fun(tmpx, tmpy, board, player) {
 						verScore++
 						verSpace++
 					}
@@ -79,8 +79,8 @@ func heuristic2(board [][]int, player int) int {
 				tmpy = y
 				diagScore1 := 0
 				diagSpace1 := 0
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx-1, tmpy-1 {
-					if isMeNew(tmpx, tmpy, board, player) {
+				for ; isValidCoord(tmpx, tmpy) && (fun(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx-1, tmpy-1 {
+					if fun(tmpx, tmpy, board, player) {
 						diagScore1++
 						diagSpace1++
 					}
@@ -90,8 +90,8 @@ func heuristic2(board [][]int, player int) int {
 				}
 				tmpx = x
 				tmpy = y
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx+1, tmpy+1 {
-					if isMeNew(tmpx, tmpy, board, player) {
+				for ; isValidCoord(tmpx, tmpy) && (fun(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx+1, tmpy+1 {
+					if fun(tmpx, tmpy, board, player) {
 						diagScore1++
 						diagSpace1++
 					}
@@ -103,8 +103,8 @@ func heuristic2(board [][]int, player int) int {
 				tmpy = y
 				diagScore2 := 0
 				diagSpace2 := 0
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx+1, tmpy-1 {
-					if isMeNew(tmpx, tmpy, board, player) {
+				for ; isValidCoord(tmpx, tmpy) && (fun(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx+1, tmpy-1 {
+					if fun(tmpx, tmpy, board, player) {
 						diagScore2++
 						diagSpace2++
 					}
@@ -114,8 +114,8 @@ func heuristic2(board [][]int, player int) int {
 				}
 				tmpx = x
 				tmpy = y
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx-1, tmpy+1 {
-					if isMeNew(tmpx, tmpy, board, player) {
+				for ; isValidCoord(tmpx, tmpy) && (fun(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx-1, tmpy+1 {
+					if fun(tmpx, tmpy, board, player) {
 						diagScore2++
 						diagSpace2++
 					}
@@ -123,13 +123,7 @@ func heuristic2(board [][]int, player int) int {
 						diagSpace2++
 					}
 				}
-				/*
-					if verScore > horScore && verSpace >= 4 {
-						b[x][y] = verScore
-					} else if horScore > verScore && horSpace >= 4 {
-						b[x][y] = horScore
-					}*/
-				log.Println(x, y, "horScore= ", horScore, "verScore= ", verScore, "dia1 score= ", diagScore1, "dia2score = ", diagScore2, "horspace= ", horSpace, "verspace= ", verSpace, "dia1space= ", diagSpace1, "diagspace2= ", diagSpace2)
+				//log.Println(x, y, "horScore= ", horScore, "verScore= ", verScore, "dia1 score= ", diagScore1, "dia2score = ", diagScore2, "horspace= ", horSpace, "verspace= ", verSpace, "dia1space= ", diagSpace1, "diagspace2= ", diagSpace2)
 				if horSpace < 4 {
 					horScore = 0
 				}
@@ -146,143 +140,18 @@ func heuristic2(board [][]int, player int) int {
 			}
 		}
 	}
-	log.Println("_________")
 	for x := 0; x < WIDTH; x++ {
-		log.Println(board[x])
 		for y := 0; y < HEIGHT; y++ {
 			score += b[x][y]
 			b[x][y] = 0
 		}
 	}
-	log.Println("_________")
-	log.Fatal("")
-	//ENNEMY
-	enemyScore := 0
-	for x := 0; x < WIDTH; x++ {
-		for y := 0; y < HEIGHT; y++ {
-			if isMeNew(x, y, board, player) {
-				tmpx := x
-				tmpy := y
-				horScore := 0
-				horSpace := 0
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx++ {
-					if isMeNew(tmpx, tmpy, board, player) {
-						horScore++
-						horSpace++
-					}
-					if isEmptyNew(tmpx, tmpy, board, player) {
-						horSpace++
-					}
-				}
-				tmpx = x
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx-- {
-					if isMeNew(tmpx, tmpy, board, player) {
-						horScore++
-						horSpace++
-					}
-					if isEmptyNew(tmpx, tmpy, board, player) {
-						horSpace++
-					}
-				}
-				tmpx = x
-				verScore := 0
-				verSpace := 0
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpy++ {
-					if isMeNew(tmpx, tmpy, board, player) {
-						verScore++
-						verSpace++
-					}
-					if isEmptyNew(tmpx, tmpy, board, player) {
-						verSpace++
-					}
-				}
-				tmpy = y
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpy-- {
-					if isMeNew(tmpx, tmpy, board, player) {
-						verScore++
-						verSpace++
-					}
-					if isEmptyNew(tmpx, tmpy, board, player) {
-						verSpace++
-					}
-				}
-				tmpy = y
-				diagScore1 := 0
-				diagSpace1 := 0
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx-1, tmpy-1 {
-					if isMeNew(tmpx, tmpy, board, player) {
-						diagScore1++
-						diagSpace1++
-					}
-					if isEmptyNew(tmpx, tmpy, board, player) {
-						diagSpace1++
-					}
-				}
-				tmpx = x
-				tmpy = y
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx+1, tmpy+1 {
-					if isMeNew(tmpx, tmpy, board, player) {
-						diagScore1++
-						diagSpace1++
-					}
-					if isEmptyNew(tmpx, tmpy, board, player) {
-						diagSpace1++
-					}
-				}
-				tmpx = x
-				tmpy = y
-				diagScore2 := 0
-				diagSpace2 := 0
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx+1, tmpy-1 {
-					if isMeNew(tmpx, tmpy, board, player) {
-						diagScore2++
-						diagSpace2++
-					}
-					if isEmptyNew(tmpx, tmpy, board, player) {
-						diagSpace2++
-					}
-				}
-				tmpx = x
-				tmpy = y
-				for ; isValidCoord(tmpx, tmpy) && (isMeNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx-1, tmpy+1 {
-					if isMeNew(tmpx, tmpy, board, player) {
-						diagScore2++
-						diagSpace2++
-					}
-					if isEmptyNew(tmpx, tmpy, board, player) {
-						diagSpace2++
-					}
-				}
-				/*
-					if verScore > horScore && verSpace >= 4 {
-						b[x][y] = verScore
-					} else if horScore > verScore && horSpace >= 4 {
-						b[x][y] = horScore
-					}*/
-				if horSpace < 4 {
-					log.Fatal("NO SPACE")
-					horScore = 0
-				}
-				if verSpace < 4 {
-					log.Fatal("NO SPACE")
-					verScore = 0
-				}
-				if diagSpace1 < 4 {
-					log.Fatal("NO SPACE")
-					diagScore1 = 0
-				}
-				if diagSpace2 < 4 {
-					log.Fatal("NO SPACE")
-					diagScore2 = 0
-				}
-				b[x][y] = maxval(horScore, verScore, diagScore1, diagScore2)
-			}
-		}
-	}
-	for x := 0; x < WIDTH; x++ {
-		for y := 0; y < HEIGHT; y++ {
-			enemyScore += b[x][y]
-		}
-	}
+	return score
+
+}
+
+func heuristic2(board [][]int, player int) int {
+	score := getScore(board, player, isMeNew)
+	enemyScore := getScore(board, player, isEnemyNew)
 	return score - enemyScore
 }
