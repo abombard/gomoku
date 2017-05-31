8eb240b8e7ad65d57e69a49dfaa0ece8c69f28e6
abombard <arthbab@gmail.com>
2017-05-31 11:10:13 +0200

    heuristic

--- heuristic_abombard.go
+++ heuristic_abombard.go
@@ -1,5 +1,50 @@
 package main
 
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func addScore(sb [][]int, x, y int, checks [][2]int) int {
+
+	score := 0
+
+	for i := range checks {
+		x1, y1 := x+checks[i][0], y+checks[i][1]
+		if isValidCoord(x1, y1) {
+			if sb[x1][y1] > 0 {
+				score += sb[x1][y1] + 1
+			} else {
+				score += sb[x1][y1] - 1
+			}
+		}
+	}
+
+	return score
+}
+
+func assignScore(sb [][]int, x, y int, me bool, checks [][2]int) int {
+
+	score := 0
+
+	for i := range checks {
+		x1, y1 := x+checks[i][0], y+checks[i][1]
+		if isValidCoord(x1, y1) {
+			if abs(sb[x1][y1]) >= abs(score) {
+				if me && sb[x1][y1] >= 0 {
+					score = sb[x1][y1] + 1
+				} else if !me && sb[x1][y1] <= 0 {
+					score = sb[x1][y1] - 1
+				}
+			}
+		}
+	}
+
+	return score
+}
+
 func heuristic(board [][]int, player int) int {
 
 	// heuristic score
@@ -20,7 +65,6 @@ func heuristic(board [][]int, player int) int {
 		for y := range board[x] {
 			if isEmptyNew(x, y, board, player) {
 				sb[x][y] = 0
-				score += addScore(sb, x, y, checksLeft)
 			} else {
 				sb[x][y] = assignScore(sb, x, y,
 					isMeNew(x, y, board, player),
