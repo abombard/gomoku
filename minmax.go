b5dfec9e34b7fdda7d28f3899076360a1deb052b
abombard <arthbab@gmail.com>
2017-05-31 11:09:56 +0200

    alphabeta

--- minmax.go
+++ minmax.go
@@ -1,10 +1,9 @@
 package main
 
-import "log"
-
-const MINMAX_ITER = 3
+const MAXDEPTH = 3
 
 func getPossibleMoveList(b [][]int) []coord {
+
 	var coords []coord
 
 	for x := 0; x < HEIGHT; x++ {
@@ -30,94 +29,80 @@ func isEnemyNew(x, y int, board [][]int, player int) bool {
 	return !isEmptyNew(x, y, board, player) && board[x][y] != player+1
 }
 
-func addScore(sb [][]int, x, y int, checks [][2]int) int {
-
-	score := 0
-
-	for i := range checks {
-		x1, y1 := x+checks[i][0], y+checks[i][1]
-		if isValidCoord(x1, y1) {
-			score += sb[x1][y1] + 1
-		}
-	}
-
-	return score
-}
-
-func assignScore(sb [][]int, x, y int, me bool, checks [][2]int) int {
-
-	score := 0
-
-	for i := range checks {
-		x1, y1 := x+checks[i][0], y+checks[i][1]
-		if isValidCoord(x1, y1) {
-			if me && sb[x1][y1] > 0 {
-				score += sb[x1][y1] + 1
-			}
-		}
-	}
-
-	return score
-}
-
 type step struct {
 	coord coord
 	score int
 }
 
-func min(scores []step) step {
-	ret := step{score: 10000}
-	for i := range scores {
-		if scores[i].score < ret.score {
-			ret = scores[i]
-		}
+func min(s1, s2 step) step {
+	if s1.score <= s2.score {
+		return s1
 	}
-	return ret
+	return s2
 }
 
-func max(scores []step) step {
-	ret := step{score: -10000}
-	for i := range scores {
-		if scores[i].score > ret.score {
-			ret = scores[i]
-		}
+func max(s1, s2 step) step {
+	if s1.score >= s2.score {
+		return s1
 	}
-	return ret
+	return s2
 }
 
-func recminmax(board [][]int, pt coord, player int, iter int) step {
+func recminmax(board [][]int, pt coord, player int, depth int, alpha, beta int) step {
+
+	player = (player + 1) % 2
 
 	board[pt.X][pt.Y] = player + 1
 
-	if iter == MINMAX_ITER {
+	next := getPossibleMoveList(board)
+	if depth == 0 || len(next) == 0 {
 		score := heuristic(board, player)
 		board[pt.X][pt.Y] = 0
 		return step{pt, score}
 	}
 
-	next := getPossibleMoveList(board)
-
-	var scores []step
-	for i := range next {
-		s := recminmax(board, next[i], (player+1)%2, iter+1)
-		scores = append(scores, step{coord: pt, score: s.score})
-	}
-
-	var ret step
-	if iter%2 == 0 {
-		ret = max(scores)
+	var v step
+	if player != current {
+		v = step{score: -10000}
+		for i := range next {
+			v = max(v, recminmax(board, next[i], player, depth-1, alpha, beta))
+			if v.score > alpha {
+				alpha = v.score
+			}
+			if alpha >= beta {
+				break
+			}
+		}
 	} else {
-		ret = min(scores)
+		v = step{score: 10000}
+		for i := range next {
+			v = min(v, recminmax(board, next[i], player, depth-1, alpha, beta))
+			if v.score < beta {
+				beta = v.score
+			}
+			if alpha >= beta {
+				break
+			}
+		}
 	}
 
 	board[pt.X][pt.Y] = 0
 
-	return ret
+	return step{pt, v.score}
 }
 
 func minmaxRoutine(board [][]int, pt coord, player int, ch chan step) {
-	ret := recminmax(board, pt, player, 0)
-	ch <- ret
+
+	// create a new slice for each go routine
+	b := make([][]int, HEIGHT)
+	for i := 0; i < HEIGHT; i++ {
+		b[i] = make([]int, WIDTH)
+		for j := 0; j < WIDTH; j++ {
+			b[i][j] = g.Board[i][j]
+		}
+	}
+
+	ch <- recminmax(b, pt, player, MAXDEPTH, -10000, 10000)
 }
 
 func minmax(board [HEIGHT][WIDTH]int, player int) coord {
@@ -135,26 +120,14 @@ func minmax(board [HEIGHT][WIDTH]int, player int) coord {
 
 	for x := 0; x < len(nextMoves); x++ {
 
-		// create a new slice for each go routine
-		b := make([][]int, HEIGHT)
-		for i := 0; i < HEIGHT; i++ {
-			b[i] = make([]int, WIDTH)
-			for j := 0; j < WIDTH; j++ {
-				b[i][j] = g.Board[i][j]
-			}
-		}
-
 		go minmaxRoutine(b, nextMoves[x], player, ch)
 	}
 
-	res := make([]step, 0)
+	v := step{score: -10000}
 	for i := 0; i < len(nextMoves); i++ {
 		tmp := <-ch
-		res = append(res, tmp)
-		log.Println(res)
+		v = max(v, tmp)
 	}
 
-	ret := min(res)
-
-	return ret.coord
+	return v.coord
 }
