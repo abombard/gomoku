07931c24bdd1e4089bdc1a5c203e3be8611ad47a
abombard <arthbab@gmail.com>
2017-06-01 18:48:30 +0200

    parcourt tableau 1 fois

--- heuristic_aduban.go
+++ heuristic_aduban.go
@@ -201,6 +201,184 @@ func getScore(board [][]int, player int) int {
 				}
 
 				score += horScore + verScore + diagScore1 + diagScore2
+			} else if isEnemyNew(x, y, board, player) {
+				tmpx := x + 1
+				tmpy := y
+				horScore := 0
+				horSpace := 0
+				verScore := 0
+				verSpace := 0
+				diagScore1 := 0
+				diagSpace1 := 0
+				diagScore2 := 0
+				diagSpace2 := 0
+				b := 1
+				stop := false
+				for ; isValidCoord(tmpx, tmpy) && (isEnemyNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx++ {
+					if !stop && isEnemyNew(tmpx, tmpy, board, player) {
+						if canBeCaptured(board, tmpx, tmpy, player) {
+							b = 1
+						}
+						horScore += b
+						b++
+					} else {
+						stop = true
+					}
+
+					horSpace++
+					if horSpace > 5 {
+						break
+					}
+				}
+				stop = false
+				tmpx = x - 1
+				for ; isValidCoord(tmpx, tmpy) && (isEnemyNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx-- {
+					if !stop && isEnemyNew(tmpx, tmpy, board, player) {
+						if canBeCaptured(board, tmpx, tmpy, player) {
+							b = 1
+						}
+						horScore += b
+						b++
+					} else {
+						stop = true
+					}
+
+					horSpace++
+					if horSpace > 5 {
+						break
+					}
+				}
+				stop = false
+				tmpx = x
+				tmpy = y + 1
+				b = 1
+				for ; isValidCoord(tmpx, tmpy) && (isEnemyNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpy++ {
+					if !stop && isEnemyNew(tmpx, tmpy, board, player) {
+						if canBeCaptured(board, tmpx, tmpy, player) {
+							b = 1
+						}
+						verScore += b
+						b++
+					} else {
+						stop = true
+					}
+					verSpace++
+					if verSpace > 5 {
+						break
+					}
+				}
+				stop = false
+				tmpy = y - 1
+				for ; isValidCoord(tmpx, tmpy) && (isEnemyNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpy-- {
+					if !stop && isEnemyNew(tmpx, tmpy, board, player) {
+						if canBeCaptured(board, tmpx, tmpy, player) {
+							b = 1
+						}
+						verScore += b
+						b++
+					} else {
+						stop = true
+					}
+
+					verSpace++
+					if verSpace > 5 {
+						break
+					}
+				}
+				stop = false
+				b = 1
+				tmpy = y - 1
+				tmpx = x - 1
+				for ; isValidCoord(tmpx, tmpy) && (isEnemyNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx-1, tmpy-1 {
+					if !stop && isEnemyNew(tmpx, tmpy, board, player) {
+						if canBeCaptured(board, tmpx, tmpy, player) {
+							b = 1
+						}
+						diagScore1 += b
+						b++
+					} else {
+						stop = true
+					}
+
+					diagSpace1++
+					if diagSpace1 > 5 {
+						break
+					}
+				}
+				stop = false
+				tmpx = x + 1
+				tmpy = y + 1
+				for ; isValidCoord(tmpx, tmpy) && (isEnemyNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx+1, tmpy+1 {
+					if !stop && isEnemyNew(tmpx, tmpy, board, player) {
+						if canBeCaptured(board, tmpx, tmpy, player) {
+							b = 1
+						}
+						diagScore1 += b
+						b++
+					} else {
+						stop = true
+					}
+
+					diagSpace1++
+					if diagSpace1 > 5 {
+						break
+					}
+				}
+				stop = false
+				b = 1
+				tmpx = x + 1
+				tmpy = y - 1
+				for ; isValidCoord(tmpx, tmpy) && (isEnemyNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx+1, tmpy-1 {
+					if !stop && isEnemyNew(tmpx, tmpy, board, player) {
+						if canBeCaptured(board, tmpx, tmpy, player) {
+							b = 1
+						}
+						diagScore2 += b
+						b++
+					} else {
+						stop = true
+					}
+
+					diagSpace2++
+					if diagSpace2 > 5 {
+						break
+					}
+				}
+				stop = false
+				tmpx = x - 1
+				tmpy = y + 1
+				for ; isValidCoord(tmpx, tmpy) && (isEnemyNew(tmpx, tmpy, board, player) || isEmptyNew(tmpx, tmpy, board, player)); tmpx, tmpy = tmpx-1, tmpy+1 {
+					if !stop && isEnemyNew(tmpx, tmpy, board, player) {
+						if canBeCaptured(board, tmpx, tmpy, player) {
+							b = 1
+						}
+						diagScore2 += b
+						b++
+					} else {
+						stop = true
+					}
+
+					diagSpace2++
+					if diagSpace2 > 5 {
+						break
+					}
+				}
+				//log.Println(x, y, "horScore= ", horScore, "verScore= ", verScore, "dia1 score= ", diagScore1, "dia2score = ", diagScore2, "horspace= ", horSpace, "verspace= ", verSpace, "dia1space= ", diagSpace1, "diagspace2= ", diagSpace2)
+
+				if horSpace < 4 {
+					horScore = 0
+				}
+				if verSpace < 4 {
+					verScore = 0
+				}
+				if diagSpace1 < 4 {
+					diagScore1 = 0
+				}
+				if diagSpace2 < 4 {
+					diagScore2 = 0
+				}
+
+				score -= horScore + verScore + diagScore1 + diagScore2
 			}
 		}
 	}
@@ -209,16 +387,9 @@ func getScore(board [][]int, player int) int {
 
 func heuristic2(board [][]int) int {
 	score := getScore(board, current)
-	enemyScore := getScore(board, (current+1)%2)
-	var scoreFinal int
-	if MAXDEPTH%2 == 1 {
-		scoreFinal = score - enemyScore
-	} else {
-		scoreFinal = enemyScore - score
-	}
 	/*
 		log.Println("heuristic", scoreFinal)
 		printBoard(board)
 	*/
-	return scoreFinal
+	return score
 }
