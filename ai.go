80be89a474a0b1e6307728097dc2768dedaeaa7f
abombard <arthbab@gmail.com>
2017-06-02 14:53:38 +0200

    try another getPossibleMove: //commented

--- ai.go
+++ ai.go
@@ -5,16 +5,78 @@ func aiPlay() coord {
 	return coord
 }
 
-func isPawnNearby(b [][]int, xtarg, ytarg int) bool {
+func isPawnNearby(b [][]int, x0, y0 int) bool {
+
 	for x := -1; x < 2; x++ {
 		for y := -1; y < 2; y++ {
-			if isValidCoord(xtarg+x, ytarg+y) {
-				if b[xtarg+x][ytarg+y] != 0 {
+			if isValidCoord(x0+x, y0+y) {
+				if b[x0+x][y0+y] != 0 {
 					return true
 				}
 			}
 
 		}
 	}
+
+	return false
+}
+
+func check(c1, c2 int) bool {
+	if !isEmpty(c1) && !isEmpty(c2) && c1 == c2 {
+		return true
+	}
+	return false
+}
+
+func isPawnNearby2(b [][]int, x0, y0 int) bool {
+
+	if isValidCoord(x0-1, y0-1) && isValidCoord(x0-2, y0-2) {
+		if check(b[x0-1][y0-1], b[x0-2][y0-2]) {
+			return true
+		}
+	}
+
+	if isValidCoord(x0-1, y0) && isValidCoord(x0-2, y0) {
+		if check(b[x0-1][y0], b[x0-2][y0]) {
+			return true
+		}
+	}
+
+	if isValidCoord(x0-1, y0+1) && isValidCoord(x0-2, y0+2) {
+		if check(b[x0-1][y0+1], b[x0-2][y0+2]) {
+			return true
+		}
+	}
+
+	if isValidCoord(x0, y0-1) && isValidCoord(x0, y0-2) {
+		if check(b[x0][y0-1], b[x0][y0-2]) {
+			return true
+		}
+	}
+
+	if isValidCoord(x0, y0+1) && isValidCoord(x0, y0+2) {
+		if check(b[x0][y0+1], b[x0][y0+2]) {
+			return true
+		}
+	}
+
+	if isValidCoord(x0+1, y0-1) && isValidCoord(x0+2, y0-2) {
+		if check(b[x0+1][y0-1], b[x0+2][y0-2]) {
+			return true
+		}
+	}
+
+	if isValidCoord(x0+1, y0) && isValidCoord(x0+2, y0) {
+		if check(b[x0+1][y0], b[x0+2][y0]) {
+			return true
+		}
+	}
+
+	if isValidCoord(x0+1, y0+1) && isValidCoord(x0+2, y0+2) {
+		if check(b[x0+1][y0+1], b[x0+2][y0+2]) {
+			return true
+		}
+	}
+
 	return false
 }
